51f1b994dbdfa804b2176abc6b713594b11e4717
Mohnish Tiwari <37740231+JJdeveloperz@users.noreply.github.com>
2020-05-01 04:26:22 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+(function ($) {
+    "use strict";
+
+
+
+
+})(jQuery);
+
+
+
+function fn() {
+  var table=document.getElementById("mytable");
+  var row=table.insertRow(-1);
+  var cell1=row.insertCell(0);
+  var cell2=row.insertCell(1);
+  cell1.innerHTML="a";
+  cell2.innerHTML="b";
+
+}
